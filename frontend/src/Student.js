107c3220e42517571b8ac958b2920fc983190f85
Madhara Weerasinghe <111627132+MadharaV@users.noreply.github.com>
2025-04-06 16:01:21 +0530

    create student.js update server.js update App.js

---
+++ frontend/src/Student.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Student() {
+  const [student, setStudent] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8081/")
+      .then((res) => setStudent(res.data))
+      .catch((err) => console.log(err));
+  });
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <button className="btn btn-success">Add +</button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {student.map((data, i) => (
+              <tr key={i}>
+                <td>{data.Name}</td>
+                <td>{data.Email}</td>
+                <td>
+                  <button className="btn btn-primary">Update</button>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Student;
